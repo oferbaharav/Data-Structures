54b6d7f7debfdf5854ff6f39f28368fa892d1421
Ofer Baharav <oferbaharav@gmail.com>
2020-09-24 22:25:58 -0700

    module 3 sprint 2

---
+++ binary_search_tree/queue.py
@@ -0,0 +1,43 @@
+"""
+A queue is a data structure whose primary purpose is to store and
+return elements in First In First Out order. 
+
+1. Implement the Queue class using an array as the underlying storage structure.
+   Make sure the Queue tests pass.
+2. Re-implement the Queue class, this time using the linked list implementation
+   as the underlying storage structure.
+   Make sure the Queue tests pass.
+3. What is the difference between using an array vs. a linked list when 
+   implementing a Queue?
+   
+Stretch: What if you could only use instances of your Stack class to implement the Queue?
+         What would that look like? How many Stacks would you need? Try it!
+"""
+from singly_linked_list import LinkedList  
+
+class Queue:
+    def __init__(self):
+        self.size = 0
+        # self.storage = ?
+        self.storage = LinkedList()
+    
+    def __len__(self):
+        pass
+        return self.size
+
+    def enqueue(self, value):
+        pass
+        self.storage.add_to_tail(value)
+        self.size +=1 
+            
+
+    def dequeue(self):
+        pass
+        if self.size == 0:
+            return None
+        value = self.storage.remove_head()
+        self.size -=1
+        return value
+        
+#3. instead of using append and pop(0) on python lists
+# I'm using add_to_tail and remove_head on linked LinkedListTests
